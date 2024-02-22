c5532b2858b107f0ab3db6a9b6d02d6655ac3fa2
Alice <alicevteixeira@gmail.com>
2024-02-22 10:45:12 +0000

    repo_preparation

---
+++ scripts/script.py
@@ -0,0 +1,9 @@
+import sys
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("demo").getOrCreate()
+
+df = spark.read.json("./WorldExpenditures.csv")
+
+df.where("age > 21").select("name.first").show()
+
