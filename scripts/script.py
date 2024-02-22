a5f77fcb90f7fdeb8cf0ca714bc78e0725664d2d
Alice <alicevteixeira@gmail.com>
2024-02-22 11:12:17 +0000

    samesies as before

--- scripts/script.py
+++ scripts/script.py
@@ -8,11 +8,11 @@ spark = SparkSession.builder.appName("demo").getOrCreate()
 
 file_location = "./data/WorldExpenditures.csv"
 
-
+#Pandas dataframe
 panda_df = pd.read_csv(file_location, usecols= ['Year', 'Country' ,'Sector', 'Expenditure(million USD)', 'GDP(%)'])
 print (panda_df)
 
-
+#Pyspark dataframe
 df = spark.createDataFrame(panda_df)
 print(df)
 
