1612e689d1abf76db865d74cb215602f91e7b4e2
Alice <alicevteixeira@gmail.com>
2024-02-22 11:11:50 +0000

    load to pandas dataframe to load it in spark dataframe

--- scripts/script.py
+++ scripts/script.py
@@ -1,11 +1,19 @@
 import sys
+import pandas as pd
 from pyspark.sql import SparkSession
+
 
 spark = SparkSession.builder.appName("demo").getOrCreate()
 
-df = spark.read.csv("./data/WorldExpenditures.csv")
-df.printSchema()
+
+file_location = "./data/WorldExpenditures.csv"
 
 
+panda_df = pd.read_csv(file_location, usecols= ['Year', 'Country' ,'Sector', 'Expenditure(million USD)', 'GDP(%)'])
+print (panda_df)
+
+
+df = spark.createDataFrame(panda_df)
+print(df)
 
 
