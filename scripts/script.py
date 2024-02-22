35ca341001e0d037adb01257b6d403ac9925c59a
Alice <alicevteixeira@gmail.com>
2024-02-22 10:53:27 +0000

    correct format script

--- scripts/script.py
+++ scripts/script.py
@@ -5,6 +5,7 @@ spark = SparkSession.builder.appName("demo").getOrCreate()
 
 df = spark.read.csv("./data/WorldExpenditures.csv")
 df.printSchema()
-#df.where("age > 21").select("name.first").show()
+
+
 
 
